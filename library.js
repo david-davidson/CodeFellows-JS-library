1690c581f09cf35ec8bb82d23681673095c7bb8e
david-davidson <david.davidson@logos.com>
2014-07-15 11:38:30 -0700

    Possible fix!

--- library.js
+++ library.js
@@ -9,7 +9,7 @@ function Library(name) {
         for (var shelf in this.shelves) {
             thisShelf = this.shelves[shelf];
             for (var i = 0; i < thisShelf.books.length; i++) {
-                allShelvedBooks[thisShelf.books[i].title] = thisShelf.name;
+                allShelvedBooks[thisShelf.books[i]] = thisShelf.name;
             }
         }
         return allShelvedBooks;
@@ -27,40 +27,42 @@ function Book(shelf, title, author) {
     // Use example: var hamlet = new Book(fiction, 'Hamlet', 'Shakespeare');
     this.title = title;
     this.author = author;
-
     this.unshelf = function() {
-        console.log('Removing ' + this.title + ' from ' + shelf.name);
-        var i = shelf.books.indexOf(this);
-        shelf.books.splice(i, 1);
+        shelf = this.shelf;
+        console.log('Removing ' + this.title + ' from ' + this.shelf.name);
+        var i = this.shelf.books.indexOf(this);
+        this.shelf.books.splice(i, 1);
         this.shelved = false;
     };
-
     this.enshelf = function(shelf) {
         if (this.shelved) {
-            this.unshelf(); // Don't try to unshelve books that aren't shelved
+            this.unshelf();
         }
-        shelf.books.push(this);
+        shelf.books.push(this.title); // We push only the title, not the entire book object: otherwise, each book refers to its shelf, which refers to all its books, each of which refers to its shelf, each of which refers to all its books, etc. Note that, therefore, a given shelf knows only the *names* of its books.
         this.shelved = true;
-        this.shelf = shelf; // Here's our problem!!!
+        this.shelf = shelf; // Update the current shelf
     };
     this.enshelf(shelf);
 }
 
+// Initialize library
 var babel = new Library('babel');
+
+// Initialize shelves
 var memoir = new Shelf(babel, 'memoir');
 var nonfiction = new Shelf(babel, 'nonfiction');
 var fiction = new Shelf(babel, 'fiction');
+
+// Initialize books
 var elAleph = new Book(fiction, 'El Aleph', 'Borges');
 var elHacedor = new Book(memoir, 'El Hacedor', 'Borges');
 var otrasInquisiciones = new Book(nonfiction, 'Otras Inquisiciones', 'Borges');
+
+// Test shelf, enshelf methods
 otrasInquisiciones.enshelf(memoir);
 otrasInquisiciones.enshelf(nonfiction);
 otrasInquisiciones.enshelf(fiction);
 otrasInquisiciones.unshelf();
 
+// Test library's reporting method
 console.log(babel.report());
-
-/*
-To unshelf() itself, it needs to know what shelf it's on. If we set shelf as an attribute of Book(), then each shelf holds an array of books, each of which points to its parent shelf, each of which holds... infinite back and forth. Only one should know, and it's probably shelf. (Cleaner: library knows shelves; shelves know books; books know only about themselves.) Maybe something like getParentShelf(), which could ask each shelf, Do you hold me? loop through list of shelves, return shelf that holds books
-
-*/
